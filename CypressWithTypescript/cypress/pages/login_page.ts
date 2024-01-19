8800217dce104b4df469ba1dca0743e8fe485864
Gilbert Langat <Gilbert.Langat@dentsu.com>
2024-01-19 16:07:10 +0300

    Added comments

--- CypressWithTypescript/cypress/pages/login_page.ts
+++ CypressWithTypescript/cypress/pages/login_page.ts
@@ -1,17 +1,26 @@
+// Export the LoginPage class
 export class LoginPage {
 
     loginPage_username = '#uname'
     loginPage_password = '#pwd'
     loginPage_loginButton = '[type="submit"]'
+
+    // Method to navigate to the login page
     navigate(url) {
         cy.visit (url)
     }
-     enterUsername(username: string){
+     
+    // Method to enter the username
+    enterUsername(username: string){
         cy.get(this.loginPage_username).type(username)
      }
+
+     // Method to enter the password
      enterPassword(password: string) {
         cy.get(this.loginPage_password).type(password)
      }
+     
+     // Method to click the login button
      clickLogin(){
         cy.get(this.loginPage_loginButton).click();
      }
